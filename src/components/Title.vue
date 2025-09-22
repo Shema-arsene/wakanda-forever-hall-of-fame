ed80e43a884099c3f6e5cbed8224fc635dd2871c
Shema Arsene <aniyonkurushema@gmail.com>
2025-09-22 11:10:30 +0200

    Add new font(s) and conditional text sizing

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,5 +1,13 @@
 <script setup>
 import { motion } from "motion-v"
+import { defineProps } from "vue"
+
+defineProps({
+  isHeroSection: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
 
 <template>
@@ -11,12 +19,14 @@ import { motion } from "motion-v"
   >
     <p
       class="font-panthera tracking-[0.3em] flex items-start justify-between text-yellow-100 drop-shadow-[0_0_15px_rgba(34,197,94,0.8)]"
+      :class="isHeroSection === true && 'text-4xl justify-center'"
     >
       <span class="text-1xl drop-shadow-[0_0_15px_rgba(34,197,94,0.8)]"
         >The</span
       >
       <span
-        class="text-3xl font-extrabold drop-shadow-[0_0_15px_rgba(34,197,94,0.8)]"
+        class="hall-text text-3xl font-extrabold drop-shadow-[0_0_15px_rgba(34,197,94,0.8)]"
+        :class="isHeroSection === true && 'text-9xl'"
         >Hall</span
       >
       <span
@@ -25,9 +35,31 @@ import { motion } from "motion-v"
       >
     </p>
     <h1
-      class="text-3xl sm:text-4xl font-extrabold text-yellow-100 drop-shadow-[0_0_15px_rgba(34,197,94,0.8)]"
+      class="zero-limit-text text-3xl sm:text-4xl font-extrabold text-yellow-100 drop-shadow-[0_0_15px_rgba(34,197,94,0.8)]"
+      :class="
+        isHeroSection === true && 'zero-limit-text-hero tracking-[1.5rem]'
+      "
     >
       Zero Limits
     </h1>
   </motion.div>
 </template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap");
+
+.zero-limit-text {
+  font-family: "Oswald", sans-serif;
+  font-weight: 900;
+}
+
+.zero-limit-text-hero {
+  font-size: 5rem;
+}
+
+.hall-text {
+  font-family: "Alegreya", serif;
+  font-weight: 900;
+}
+</style>
